3f9e6fcf8d56b6be09a81c86cb9f6ca461768c22
Scavenger233 <liyiminginireland@gmail.com>
2025-05-29 18:14:12 +0100

    Initial Springboot commit

---
+++ src/main/java/com/ecomvision/backend/EcomvisionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecomvision.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomvisionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomvisionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomvision/backend/EcomvisionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomvision.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomvisionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
